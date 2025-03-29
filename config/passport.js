617871c8115a6a7c2ba640f725a3dbe67b70f2f0
Track404 <111713000+Track404@users.noreply.github.com>
2025-03-29 20:46:21 +0100

    put the github callback for db in .env

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/github/callback',
+      callbackURL: process.env.GIHUB_CALLBACK_URL,
       scope: ['user:email'],
     },
     async (accessToken, refreshToken, profile, done) => {
